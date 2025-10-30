d3da74172e4fc5db4f99bea613548c83125b52a0
Viktor Kalistratov <viktor.calistratov27@gmail.com>
2025-10-30 11:32:52 +0300

    Начинаю с чистого листа

--- backend/src/config.js
+++
@@ -1,7 +0,0 @@
-const DEFAULT_PORT = 8080;
-
-export const config = {
-  port: Number.parseInt(process.env.PORT ?? '', 10) || DEFAULT_PORT,
-};
-
-export default config;
--- shared/ws.js
+++
@@ -1,9 +0,0 @@
-/**
- * @template T
- * @typedef {Object} WS
- * @property {string} type
- * @property {string} [rid]
- * @property {number} ts
- * @property {T} payload
- */
-export {};
